b5a99fe3280d6c59ec811ad0ad302fa48c8b824c
Abdull6kh <pbgpbg016@gmail.com>
2022-06-07 17:12:41 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var elCard = document.querySelector('.table__info');
+var elCard2 = document.querySelector('.table__info2');
+var elCard3 = document.querySelector('.table__info3');
+var elButton = document.querySelector('.burger-btn');
+var elMenu = document.querySelector('.menu');
+
+elButton.addEventListener('click', function(){
+    elMenu.classList.toggle("menu--open")
+})
+
+elCard.addEventListener('click', function(){
+    elCard.classList.toggle("info--open")
+})
+elCard2.addEventListener('click', function(){
+    elCard2.classList.toggle("info--open2")
+})
+elCard3.addEventListener('click', function(){
+    elCard3.classList.toggle("info--open3")
+})
