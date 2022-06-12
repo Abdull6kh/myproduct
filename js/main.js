3d29c2a8895c96f52c24d35a986eca64c9dc4009
Abdull6kh <pbgpbg016@gmail.com>
2022-06-13 01:19:11 +0500

    correct the errors

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ elCard.addEventListener('click', function(){
     elCard.classList.toggle("info--open")
 })
 elCard2.addEventListener('click', function(){
-    elCard2.classList.toggle("info--open2")
+    elCard2.classList.toggle("table-info--open2")
 })
 elCard3.addEventListener('click', function(){
     elCard3.classList.toggle("info--open3")
